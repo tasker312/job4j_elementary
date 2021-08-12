8c7358a93e66a8347b2808f41f048ee4d49fe9d8
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-08-12 21:20:12 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+    }
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if (question.equals("Пока.")) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
